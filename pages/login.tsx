f4a32c09fbc6c665e700db89d97df99728734317
Knob <59198478+nbr41to@users.noreply.github.com>
2023-01-15 13:04:29 +0900

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,21 @@
+import { ColorGoogleIcon } from '@/common/icons';
+import { signInWithGoogle } from '@/utils/supabase/auth';
+import { Button } from '@mantine/core';
+
 const Page = () => {
   return (
-    <div>
-      <h2>Login page</h2>
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex h-80 w-80 items-center justify-center rounded border border-solid p-8">
+        <Button
+          className="border-blue-700 bg-white text-blue-700 shadow"
+          variant="outline"
+          fullWidth
+          leftIcon={<ColorGoogleIcon size={20} />}
+          onClick={() => signInWithGoogle()}
+        >
+          Googleアカウントでログイン
+        </Button>
+      </div>
     </div>
   );
 };
