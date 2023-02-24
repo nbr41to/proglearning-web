1f0422b1cccec612f81f2e428b79f421dc9a8c51
Knob <59198478+nbr41to@users.noreply.github.com>
2023-02-25 00:20:37 +0900

    contactのレスポンシブ

--- src/components/features/contact/ContactTemplate.tsx
+++ src/components/features/contact/ContactTemplate.tsx
@@ -11,12 +11,12 @@ type Props = {
 
 export const ContactTemplate: FC<Props> = ({ onSubmit }) => {
   return (
-    <div className="w-main mx-auto pt-12">
-      <SignboardContainer>
-        <div className="space-y-8 p-8">
+    <div className="w-main mx-auto pt-12 sp:-mt-3 sp:pt-0">
+      <SignboardContainer fill>
+        <div className="space-y-8 p-8 sp:p-0">
           <div>
             <PageTitle title="Contact" />
-            <p className="mt-4 whitespace-pre text-center">
+            <p className="mt-4 whitespace-pre text-center sp:whitespace-pre-wrap">
               プログラミング学習の相談からサービスに関する相談まで幅広く受け付けております。
               <wbr />
               気軽にご連絡ください。
