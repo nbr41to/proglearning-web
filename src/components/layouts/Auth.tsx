19e9361fba8fe8d81585b130f1df9185d3c16f18
Knob <59198478+nbr41to@users.noreply.github.com>
2023-03-11 17:48:11 +0900

    検証用

--- src/components/layouts/Auth.tsx
+++ src/components/layouts/Auth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import type { FC, ReactNode } from 'react';
 
 import { useGetMeStatus } from '@/hooks/apiHook/useGetMeStatus';
@@ -15,14 +16,19 @@ export const Auth: FC<Props> = ({ children }) => {
   const currentPath = router.asPath.split('#')[0].split('?')[0];
   const isIgnorePath = unprotectedRoutes.includes(currentPath);
   const isLoginPath = currentPath === '/login';
+  console.log('isIgnorePath', isIgnorePath);
+  console.log('isLoginPath', isLoginPath);
 
   useEffect(() => {
     if (isIgnorePath) return;
     if (isLoading) return;
+    console.log('data', data);
     if (data === null && !isLoginPath) {
+      console.log('redirect to login');
       router.push('/login');
     }
     if (data && isLoginPath) {
+      console.log('redirect to dashboard');
       router.push('/dashboard');
     }
   }, [data, isLoading, isIgnorePath, isLoginPath, router]);
