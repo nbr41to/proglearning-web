d48ec1d0c442db21fab58b1324b67e8323fccecf
Knob <59198478+nbr41to@users.noreply.github.com>
2023-03-11 23:09:47 +0900

    swr dev-toolの追加

--- src/components/layouts/Providers/Providers.tsx
+++ src/components/layouts/Providers/Providers.tsx
@@ -7,6 +7,7 @@ import { MantineProvider } from '@/components/layouts/Providers/MantineProvider'
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { RecoilRoot } from 'recoil';
+import { SWRDevTools } from 'swr-devtools';
 
 type Props = {
   children: ReactNode;
@@ -16,14 +17,16 @@ type Props = {
 export const Providers: FC<Props> = ({ children, initialSession }) => {
   return (
     <SessionContextProvider initialSession={initialSession}>
-      <RecoilRoot>
-        <MantineProvider>
-          <Notifications position="top-center" />
-          <SpotlightProvider>
-            <ModalsProvider>{children}</ModalsProvider>
-          </SpotlightProvider>
-        </MantineProvider>
-      </RecoilRoot>
+      <SWRDevTools>
+        <RecoilRoot>
+          <MantineProvider>
+            <Notifications position="top-center" />
+            <SpotlightProvider>
+              <ModalsProvider>{children}</ModalsProvider>
+            </SpotlightProvider>
+          </MantineProvider>
+        </RecoilRoot>
+      </SWRDevTools>
     </SessionContextProvider>
   );
 };
